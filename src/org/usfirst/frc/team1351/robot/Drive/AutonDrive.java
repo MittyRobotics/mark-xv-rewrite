af7eb3711aca2f3a16cc31c033d92b8f19d3394c
LookLotsOfPeople <LookLotsOfPeople@users.noreply.github.com>
2018-01-29 16:15:47 -0800

    Added TODOs For the Drive Auton
    Starting point for drive auton.

--- src/org/usfirst/frc/team1351/robot/Drive/AutonDrive.java
+++ src/org/usfirst/frc/team1351/robot/Drive/AutonDrive.java
@@ -1,8 +1,20 @@
 package org.usfirst.frc.team1351.robot.Drive;
 
 public class AutonDrive extends Drive {
-    public void forward(int distance) {
+    public void forward(byte distance) {
+        //TODO Use a PID Loop to Drive Forward {distance} Inches Using the Encoders
+    }
 
+    public void backwards(byte distance) {
+        //TODO Use a PID Loop to Drive Backwards {distance} Inches Using the Encoders
+    }
+
+    public void turnLeft(byte degrees) {
+        //TODO Use a PID Loop to Turn Left {degrees} Degrees Using the Gyro
+    }
+
+    public void turnRight(byte degrees) {
+        //TODO Use a PID Loop to Turn Right {degrees} Degrees Using the Gyro
     }
 
     @Override
