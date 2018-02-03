be49dcc02f35576492d1654b91db5162666d1772
LookLotsOfPeople <LookLotsOfPeople@users.noreply.github.com>
2018-02-02 16:25:08 -0800

    Started to Work on the Controller Threads
    These two threads will practically be the only threads that continiously run during TeleOp.

--- src/org/usfirst/frc/team1351/robot/Controllers/Controller.java
+++ src/org/usfirst/frc/team1351/robot/Controllers/Controller.java
@@ -1,4 +1,15 @@
 package org.usfirst.frc.team1351.robot.Controllers;
 
-class Controller {
+import java.util.HashMap;
+import java.util.Map;
+
+class Controller extends Thread {
+	volatile boolean isTeleOp;
+	Map<String, Thread> commands = new HashMap<String, Thread>();
+
+	public void init() {
+
+	}
+
+
 }
--- src/org/usfirst/frc/team1351/robot/Controllers/DriverController.java
+++ src/org/usfirst/frc/team1351/robot/Controllers/DriverController.java
@@ -1,4 +1,19 @@
 package org.usfirst.frc.team1351.robot.Controllers;
 
+import edu.wpi.first.wpilibj.XboxController;
+
 public class DriverController extends Controller {
+	XboxController controller;
+
+
+	@Override
+	public void run() {
+		this.setName("Driver Controller Thread");
+		this.setPriority(10);
+		this.setDaemon(false);
+
+		while (shouldRun) {
+
+		}
+	}
 }
