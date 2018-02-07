61ce61368e5009a463d023d3da1f75f6fc4b804a
LookLotsOfPeople <LookLotsOfPeople@users.noreply.github.com>
2018-02-06 16:27:51 -0800

    Added Base
    The ControllerOperator class now has the basic framework for controllers and commands.

--- src/org/usfirst/frc/team1351/robot/Controllers/ControllerOperator.java
+++ src/org/usfirst/frc/team1351/robot/Controllers/ControllerOperator.java
@@ -3,5 +3,18 @@ package org.usfirst.frc.team1351.robot.Controllers;
 import edu.wpi.first.wpilibj.DriverStation;
 
 public class ControllerOperator {
+	//TODO Add Controllers
+
+	static void init() {
+		Thread operatorThread = new Thread(ControllerOperator::oThread);
+		operatorThread.setName("Operator Controller Thread");
+		operatorThread.setPriority(5); //TODO Set Priority
+		operatorThread.start();
+	}
+
+	private static void oThread() {
 		while (DriverStation.getInstance().isEnabled() && DriverStation.getInstance().isOperatorControl()) {
+			//TODO Add in Controller Options / Commands
+		}
+	}
 }
