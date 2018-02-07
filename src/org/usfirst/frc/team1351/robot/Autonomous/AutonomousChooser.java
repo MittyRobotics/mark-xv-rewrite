5d3701b16c451aae5aa423068a38b72153f2e36f
LookLotsOfPeople <LookLotsOfPeople@users.noreply.github.com>
2018-02-06 17:37:40 -0800

    Initializes and Fetches Smart Dashboard Values
    The starting spot as well as the destination values are placed on the smart dashboard and then fetched at runtime. The matchConfigMessage is also fetched and converted to lowercase for use.

--- src/org/usfirst/frc/team1351/robot/Autonomous/AutonomousChooser.java
+++ src/org/usfirst/frc/team1351/robot/Autonomous/AutonomousChooser.java
@@ -1,11 +1,19 @@
 package org.usfirst.frc.team1351.robot.Autonomous;
 
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 class AutonomousChooser {
 	static void init() {
-		//TODO Initialize Driver Station Inputs
+		SmartDashboard.putNumber("Starting Spot", 0);
+		SmartDashboard.putBoolean("Scale?", false);
 	}
 
 	static void choose() {
+		String matchConfigMessage = DriverStation.getInstance().getGameSpecificMessage().toLowerCase();
+		byte startingSpotID = (byte) SmartDashboard.getNumber("Starting Spot", 0);
+		boolean targetIsScale = SmartDashboard.putBoolean("Scale?", false);
+
 		byte command;
 
 		command = 0; //TODO Change this to Driver Station Input
