6cd3e5788492abbd21b03fd4c5b9d03a7923a701
LookLotsOfPeople <LookLotsOfPeople@users.noreply.github.com>
2018-02-06 16:42:25 -0800

    Added Camera
    The camera can then be setup using the init method in the Vision class.

--- src/org/usfirst/frc/team1351/robot/Vision/Vision.java
+++ src/org/usfirst/frc/team1351/robot/Vision/Vision.java
@@ -1,4 +1,14 @@
 package org.usfirst.frc.team1351.robot.Vision;
 
+import edu.wpi.cscore.UsbCamera;
+import edu.wpi.first.wpilibj.CameraServer;
+
 public class Vision {
+	public static void init() {
+		UsbCamera camera = CameraServer.getInstance().startAutomaticCapture("camera", 0);
+		camera.setResolution(360, 280);
+		camera.setFPS(30);
+		camera.setExposureAuto();
+		camera.setWhiteBalanceAuto();
+	}
 }
