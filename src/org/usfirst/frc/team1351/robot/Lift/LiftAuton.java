07d11b3efc7da09967ada419f67af61670e18ffc
LookLotsOfPeople <LookLotsOfPeople@users.noreply.github.com>
2018-02-19 14:30:54 -0800

    Removed Breaking Extends

--- src/org/usfirst/frc/team1351/robot/Lift/LiftAuton.java
+++ src/org/usfirst/frc/team1351/robot/Lift/LiftAuton.java
@@ -1,4 +1,4 @@
 package org.usfirst.frc.team1351.robot.Lift;
 
-public class LiftAuton extends Lift {
+public class LiftAuton {
 }
--- src/org/usfirst/frc/team1351/robot/Lift/LiftTeleOp.java
+++ src/org/usfirst/frc/team1351/robot/Lift/LiftTeleOp.java
@@ -1,4 +1,4 @@
 package org.usfirst.frc.team1351.robot.Lift;
 
-public class LiftTeleOp extends Lift {
+public class LiftTeleOp {
 }
