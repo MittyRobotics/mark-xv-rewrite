9c5ceb355859044b8b7ecd1e0c179e6bfb621205
LookLotsOfPeople <LookLotsOfPeople@users.noreply.github.com>
2018-02-19 15:10:11 -0800

    Added Lift TeleOp Command

--- src/org/usfirst/frc/team1351/robot/Lift/LiftTeleOp.java
+++ src/org/usfirst/frc/team1351/robot/Lift/LiftTeleOp.java
@@ -1,4 +1,11 @@
 package org.usfirst.frc.team1351.robot.Lift;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+
 public class LiftTeleOp {
+	public static void set(double value) {
+		if (Lift.checkLimitSwitches()) {
+			Lift.set(ControlMode.PercentOutput, value);
+		}
+	}
 }
