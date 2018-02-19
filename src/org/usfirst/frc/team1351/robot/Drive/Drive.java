2d8c3db3e6fe8dd97e55594470389ce79dcf04d2
LookLotsOfPeople <LookLotsOfPeople@users.noreply.github.com>
2018-02-19 15:34:15 -0800

    Redefined Drive Class

--- src/org/usfirst/frc/team1351/robot/Drive/Drive.java
+++ src/org/usfirst/frc/team1351/robot/Drive/Drive.java
@@ -8,7 +8,7 @@ import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PIDOutput;
 import edu.wpi.first.wpilibj.PIDSource;
 
-public abstract class Drive {
+public class Drive {
 	private static final FeedbackDevice ENCODER = FeedbackDevice.QuadEncoder;
 
 	private static TalonSRX[] talons;
