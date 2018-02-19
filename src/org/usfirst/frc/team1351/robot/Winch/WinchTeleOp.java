5c788d2b06fc82058b7d77057bb96d3725882126
LookLotsOfPeople <LookLotsOfPeople@users.noreply.github.com>
2018-02-19 14:19:53 -0800

    Added in Winch Methods

--- src/org/usfirst/frc/team1351/robot/Winch/WinchTeleOp.java
+++ src/org/usfirst/frc/team1351/robot/Winch/WinchTeleOp.java
@@ -1,4 +1,33 @@
 package org.usfirst.frc.team1351.robot.Winch;
 
-public class WinchTeleOp extends Winch {
+import com.ctre.phoenix.motorcontrol.ControlMode;
+
+public class WinchTeleOp {
+	private static final float LADENOUTPUT = 0.5f;
+	private static final float UNLADENOUTPUT = 0.2f;
+	private static final float FULLFORWARDOUTPUT = 0.1f;
+
+	public static void laden() {
+		Winch.set(ControlMode.Current, LADENOUTPUT);
+	}
+
+	public static void unladen() {
+		Winch.set(ControlMode.Current, UNLADENOUTPUT);
+	}
+
+	public static void fullForward() {
+		Winch.set(ControlMode.Current, FULLFORWARDOUTPUT);
+	}
+
+	public static void negLaden() {
+		Winch.set(ControlMode.Current, -LADENOUTPUT);
+	}
+
+	public static void negUnladen() {
+		Winch.set(ControlMode.Current, -UNLADENOUTPUT);
+	}
+
+	public static void negFullForward() {
+		Winch.set(ControlMode.Current, -FULLFORWARDOUTPUT);
+	}
 }
