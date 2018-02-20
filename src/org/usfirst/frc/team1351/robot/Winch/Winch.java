d941e884b37aaee8f7d3f63d4e294d89e82948f3
LookLotsOfPeople <LookLotsOfPeople@users.noreply.github.com>
2018-02-19 16:09:00 -0800

    Added Ultrasonic Sensor

--- src/org/usfirst/frc/team1351/robot/Winch/Winch.java
+++ src/org/usfirst/frc/team1351/robot/Winch/Winch.java
@@ -3,6 +3,7 @@ package org.usfirst.frc.team1351.robot.Winch;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import edu.wpi.first.wpilibj.Ultrasonic;
 
 public class Winch {
 	private static final FeedbackDevice ENCODER = FeedbackDevice.QuadEncoder;
@@ -10,6 +11,7 @@ public class Winch {
 	private static final float WINCHSIZE = 0.5f;
 
 	private static WPI_TalonSRX[] talons;
+	private static Ultrasonic ultrasonic;
 
 
 	public static void init() {
@@ -26,12 +28,16 @@ public class Winch {
 		for (byte LEFTDRIVETALON : WINCHTALONS) {
 			talons[LEFTDRIVETALON].setInverted(true);
 		}
+
+		ultrasonic = new Ultrasonic(0, 1, Ultrasonic.Unit.kInches);
 	}
 
 	static void set(ControlMode controlMode, double value) {
 		talons[0].set(controlMode, value);
 	}
 
+	static double getUltrasonic() {
+		return ultrasonic.getRangeInches();
 	}
 
 	static float getWinchSize() {
