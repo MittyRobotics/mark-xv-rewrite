8d515138c39913ac131655c4c4f0266b52123ab5
LookLotsOfPeople <LookLotsOfPeople@users.noreply.github.com>
2018-02-19 16:09:39 -0800

    Added Climb Method

--- src/org/usfirst/frc/team1351/robot/Winch/WinchTeleOp.java
+++ src/org/usfirst/frc/team1351/robot/Winch/WinchTeleOp.java
@@ -3,5 +3,9 @@ package org.usfirst.frc.team1351.robot.Winch;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 
 public class WinchTeleOp {
+	public static void climb() {
+		if (Winch.getUltrasonic() > Winch.getWinchSize()) {
+			Winch.set(ControlMode.PercentOutput, 1);
+		}
 	}
 }
