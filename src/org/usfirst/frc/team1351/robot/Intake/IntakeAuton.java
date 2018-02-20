1110db56a7b8ae61439203f5ed82ae7a2b5c5e7f
Albert Drewke <albertdrewke18@mittymonarchs.com>
2018-02-19 16:38:54 -0800

    Created Intake Auton
    Created the Intake Auton with the methods that are required for auton.

--- src/org/usfirst/frc/team1351/robot/Intake/IntakeAuton.java
+++ src/org/usfirst/frc/team1351/robot/Intake/IntakeAuton.java
@@ -1,4 +1,35 @@
 package org.usfirst.frc.team1351.robot.Intake;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+
 public class IntakeAuton {
+
+    private static double INTAKESPEED = 0.4;
+    private static double OUTPUTSPEED = 0.4;
+    static void intake() {
+        Intake.set(ControlMode.PercentOutput, INTAKESPEED);
+    }
+
+    static void outtake() {
+        Intake.set(ControlMode.PercentOutput, OUTPUTSPEED);
+    }
+
+    static void up() {
+        Intake.extend(true);
+    }
+
+    static void down() {
+        Intake.extend(false);
+    }
+
+    static void open() {
+        Intake.close(false);
+    }
+
+    static void close() {
+        Intake.close(true);
+    }
+    public static void init() {
+
+    }
 }
