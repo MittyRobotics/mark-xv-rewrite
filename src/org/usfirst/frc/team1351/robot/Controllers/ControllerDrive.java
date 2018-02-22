812ec1168a4c9c3bdea1879280fe2564022cc0e8
LookLotsOfPeople <LookLotsOfPeople@users.noreply.github.com>
2018-02-21 17:38:54 -0800

    Made ControllerDrive Runnable
    This will soon be merged with controlleroperator to make the ENTIRE project run on only two threads unless an autonomous script is being run.

--- src/org/usfirst/frc/team1351/robot/Controllers/ControllerDrive.java
+++ src/org/usfirst/frc/team1351/robot/Controllers/ControllerDrive.java
@@ -7,56 +7,49 @@ import org.usfirst.frc.team1351.robot.Drive.DriveTeleOp;
 import static edu.wpi.first.wpilibj.GenericHID.Hand.kLeft;
 import static edu.wpi.first.wpilibj.GenericHID.Hand.kRight;
 
-class ControllerDrive {
-	private static final XboxController controller = new XboxController(0);
+class ControllerDrive implements Runnable {
+	@Override
+	public void run() {
+		XboxController xboxController = new XboxController(0);
 
-	static void init() {
-		//TODO: No C++ / Talk about the importance of teaching ::
-		Thread driverThread = new Thread(ControllerDrive::dThread);
-		driverThread.setName("Driver Controller Thread");
-		driverThread.setPriority(5); //TODO Set Priority
-		driverThread.start();
-	}
-
-	private static void dThread() {
 		while (DriverStation.getInstance().isEnabled() && DriverStation.getInstance().isOperatorControl()) {
 			//Left Side Tank Drive
-			if (controller.getY(kLeft) < -0.05 || controller.getY(kLeft) > 0.05) {
-				double yValue = controller.getY(kLeft);
+			if (xboxController.getY(kLeft) < -0.05 || xboxController.getY(kLeft) > 0.05) {
+				double yValue = xboxController.getY(kLeft);
 				DriveTeleOp.setLeft(yValue);
 			} else {
 				DriveTeleOp.setLeft(0);
 			}
 
 			//Right Side Tank Drive
-			if (controller.getY(kRight) < -0.05 || controller.getY(kRight) > 0.05) {
-				double yValue = controller.getY(kLeft);
+			if (xboxController.getY(kRight) < -0.05 || xboxController.getY(kRight) > 0.05) {
+				double yValue = xboxController.getY(kLeft);
 				DriveTeleOp.setRight(yValue);
 			} else {
 				DriveTeleOp.setRight(0);
 			}
 
 			//Manual Gear Switch
-			if (controller.getAButton()) {
+			if (xboxController.getAButton()) {
 				DriveTeleOp.setGear(1);
-			} else if (controller.getBButton()) {
+			} else if (xboxController.getBButton()) {
 				DriveTeleOp.setGear(0);
 			}
 
 			//Manual Gear Hold : Will Override Switch Mode
-			if (controller.getTriggerAxis(kRight) >= 0.2) {
+			if (xboxController.getTriggerAxis(kRight) >= 0.2) {
 				DriveTeleOp.setGear(0);
 			} else {
 				DriveTeleOp.setGear(1);
 			}
 
 			//Ramping Toggle On
-			if (controller.getXButton() && controller.getStickButton(kLeft)) {
+			if (xboxController.getXButton() && xboxController.getStickButton(kLeft)) {
 				DriveTeleOp.setRamp(0.5);
 			}
 
 			//Ramping Toggle Off
-			if (controller.getYButton() && controller.getStickButton(kLeft)) {
+			if (xboxController.getYButton() && xboxController.getStickButton(kLeft)) {
 				DriveTeleOp.setRamp(0);
 			}
 
@@ -67,6 +60,5 @@ class ControllerDrive {
 				e.printStackTrace();
 			}
 		}
-
 	}
 }
