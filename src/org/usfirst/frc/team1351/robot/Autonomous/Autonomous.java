50685c22df4d19a8d0b71c41a742df6747386d25
LookLotsOfPeople <LookLotsOfPeople@users.noreply.github.com>
2018-02-23 16:59:16 -0800

    Added JavaDocs

--- src/org/usfirst/frc/team1351/robot/Autonomous/Autonomous.java
+++ src/org/usfirst/frc/team1351/robot/Autonomous/Autonomous.java
@@ -1,5 +1,8 @@
 package org.usfirst.frc.team1351.robot.Autonomous;
 
+/**
+ * This class is meant for communicating with the other Autonomous classes.
+ */
 public class Autonomous {
 	public static void init() {
 		AutonomousChooser.init();
