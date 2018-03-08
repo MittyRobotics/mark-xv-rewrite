1deabb6b8b72a992098ae2b5b7ce2be4cb2fa716
LookLotsOfPeople <LookLotsOfPeople@users.noreply.github.com>
2018-03-07 16:02:20 -0800

    Added Autonomous Commands

--- src/org/usfirst/frc/team1351/robot/Lift/Lift.java
+++ src/org/usfirst/frc/team1351/robot/Lift/Lift.java
@@ -1,12 +1,15 @@
 package org.usfirst.frc.team1351.robot.Lift;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj.DigitalInput;
 
 class Lift {
 	private static final int[] LIFT_TALONS = {6, 7};
 	private static final int[] LIMIT_SWITCHES = {0, 1, 2};
+
+	private static final FeedbackDevice encoder = FeedbackDevice.QuadEncoder;
 
 	private static WPI_TalonSRX[] talons;
 	private static DigitalInput[] limitSwitches;
@@ -15,7 +18,7 @@ class Lift {
 	 * Initializes the lift talons and limit switches.
 	 */
 	//TODO: Set talon[0] as master
-	public static void init () {
+	public static void init() {
 		talons = new WPI_TalonSRX[LIFT_TALONS.length];
 		for (int i = 0; i < LIFT_TALONS.length; i++) {
 			talons[i] = new WPI_TalonSRX(LIFT_TALONS[i]);
@@ -29,18 +32,33 @@ class Lift {
 
 	/**
 	 * Sets the master talon to a control mode and value
+	 *
 	 * @param controlMode Control Mode that value should be run in
-	 * @param value Value that you want to set motor to
+	 * @param value       Value that you want to set motor to
 	 */
 	static void set(ControlMode controlMode, double value) {
 		talons[0].set(controlMode, value);
 	}
 
-	/** TODO: Finish this method
+	/**
+	 * TODO: Finish this method
 	 * WIP
+	 *
 	 * @return
 	 */
 	public static boolean checkLimitSwitches() {
 		return true;
+	}
+
+	static void setPIDF(double P, double I, double D) {
+		talons[0].config_kP(0, P, 0);
+		talons[0].config_kI(0, I, 0);
+		talons[0].config_kD(0, D, 0);
+		talons[0].config_kF(0, 0, 0);
+		talons[0].configSelectedFeedbackSensor(encoder, 0, 1000);
+	}
+
+	public static WPI_TalonSRX getTalonInstance() {
+		return talons[0];
 	}
 }
