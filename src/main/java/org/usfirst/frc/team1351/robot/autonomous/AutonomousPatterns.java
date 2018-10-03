64e12f1f708cbd1998958ff564e0818ed80caaa1
Ashvin Iyer <ashviniyer21@mittymonarch.com>
2018-10-02 19:19:26 -0700

    10/2/18-Auton Patterns
    Updated Auton Patterns to have Blue side in addition to Red side-Ashvin

--- src/main/java/org/usfirst/frc/team1351/robot/autonomous/AutonomousPatterns.java
+++ src/main/java/org/usfirst/frc/team1351/robot/autonomous/AutonomousPatterns.java
@@ -21,7 +21,7 @@ class AutonomousPatterns {
 		DriveAuton.move(FieldSpecifics.RED_LENGTH_FROM_LEFT_WALL_TO_SWITCH - FieldSpecifics.RED_LENGTH_FROM_LEFT_WALL_TO_CORNER_BEND - RobotSpecifics.SIDE_TO_MIDDLE - RobotSpecifics.FRONT_TO_MIDDLE);
 	}
 
-	static void redLeftForward() {
+	static void redLeftLeftForward() {
 		Intake.setArmsHardHold();
 		DriveAuton.move(FieldSpecifics.RED_LENGTH_FOR_CROSSING_AUTON_LINE - RobotSpecifics.BACK_TO_MIDDLE - RobotSpecifics.FRONT_TO_MIDDLE);
 	}
@@ -39,12 +39,12 @@ class AutonomousPatterns {
 		Intake.setArmsHardHold();
 		DriveAuton.move(FieldSpecifics.RED_LENGTH_FOR_CROSSING_AUTON_LINE/2 - RobotSpecifics.BACK_TO_MIDDLE);
 		DriveAuton.turn(90);
-		DriveAuton.move(FieldSpecifics.RED_LENGTH_OF_FIELD_FROM_DRIVER_STATION/2 - (FieldSpecifics.RED_LENGTH_FROM_LEFT_WALL_TO_SWITCH - FieldSpecifics.RED_LENGTH_FROM_LEFT_WALL_TO_CORNER_BEND + FieldSpecifics.RED_WIDTH_FOR_SWITCH/2) - FieldSpecifics.RED_LENGTH_FOR_VAULT_TO_CENTER);
+		DriveAuton.move(FieldSpecifics.RED_LENGTH_OF_FIELD_FROM_DRIVER_STATION/2 - (FieldSpecifics.RED_LENGTH_FROM_RIGHT_WALL_TO_SWITCH - FieldSpecifics.RED_LENGTH_FROM_RIGHT_WALL_TO_CORNER_BEND + FieldSpecifics.RED_WIDTH_FOR_SWITCH/2) - FieldSpecifics.RED_LENGTH_FOR_VAULT_TO_CENTER);
 		DriveAuton.turn(-90);
 		DriveAuton.move(FieldSpecifics.RED_LENGTH_FOR_CROSSING_AUTON_LINE/2 - RobotSpecifics.FRONT_TO_MIDDLE);
 	}
 
-	static void redRightForward() {
+	static void redRightRightForward() {
 		Intake.setArmsHardHold();
 		DriveAuton.move(FieldSpecifics.RED_LENGTH_FOR_CROSSING_AUTON_LINE - RobotSpecifics.BACK_TO_MIDDLE - RobotSpecifics.FRONT_TO_MIDDLE);
 	}
@@ -53,7 +53,48 @@ class AutonomousPatterns {
 		Intake.setArmsHardHold();
 		DriveAuton.move(FieldSpecifics.RED_RIGHT_LENGTH_TO_MIDDLE_OF_SWITCH - RobotSpecifics.BACK_TO_MIDDLE);
 		DriveAuton.turn(-90);
-		DriveAuton.move(FieldSpecifics.RED_LENGTH_FROM_LEFT_WALL_TO_SWITCH - FieldSpecifics.RED_LENGTH_FROM_LEFT_WALL_TO_CORNER_BEND - RobotSpecifics.SIDE_TO_MIDDLE - RobotSpecifics.FRONT_TO_MIDDLE);
+		DriveAuton.move(FieldSpecifics.RED_LENGTH_FROM_RIGHT_WALL_TO_SWITCH - FieldSpecifics.RED_LENGTH_FROM_RIGHT_WALL_TO_CORNER_BEND - RobotSpecifics.SIDE_TO_MIDDLE - RobotSpecifics.FRONT_TO_MIDDLE);
 	}
 
+	static void blueLeftLeftSwitch() {
+		Intake.setArmsHardHold();
+		DriveAuton.move(FieldSpecifics.BLUE_LEFT_LENGTH_TO_MIDDLE_OF_SWITCH - RobotSpecifics.BACK_TO_MIDDLE);
+		DriveAuton.turn(90);
+		DriveAuton.move(FieldSpecifics.BLUE_LENGTH_FROM_LEFT_WALL_TO_SWITCH - FieldSpecifics.BLUE_LENGTH_FROM_LEFT_WALL_TO_CORNER_BEND - RobotSpecifics.SIDE_TO_MIDDLE - RobotSpecifics.FRONT_TO_MIDDLE);
+	}
+
+	static void blueLeftLeftForward() {
+		Intake.setArmsHardHold();
+		DriveAuton.move(FieldSpecifics.BLUE_LENGTH_FOR_CROSSING_AUTON_LINE - RobotSpecifics.BACK_TO_MIDDLE - RobotSpecifics.FRONT_TO_MIDDLE);
+	}
+
+	static void blueMiddleLeftSwitch() {
+		Intake.setArmsHardHold();
+		DriveAuton.move(FieldSpecifics.BLUE_LENGTH_FOR_CROSSING_AUTON_LINE/2 - RobotSpecifics.BACK_TO_MIDDLE);
+		DriveAuton.turn(-90);
+		DriveAuton.move(FieldSpecifics.BLUE_LENGTH_OF_FIELD_FROM_DRIVER_STATION/2 - (FieldSpecifics.BLUE_LENGTH_FROM_LEFT_WALL_TO_SWITCH - FieldSpecifics.BLUE_LENGTH_FROM_LEFT_WALL_TO_CORNER_BEND + FieldSpecifics.BLUE_WIDTH_FOR_SWITCH/2) + FieldSpecifics.BLUE_LENGTH_FOR_VAULT_TO_CENTER);
+		DriveAuton.turn(90);
+		DriveAuton.move(FieldSpecifics.BLUE_LENGTH_FOR_CROSSING_AUTON_LINE/2 - RobotSpecifics.FRONT_TO_MIDDLE);
+	}
+
+	static void blueMiddleRightSwitch() {
+		Intake.setArmsHardHold();
+		DriveAuton.move(FieldSpecifics.BLUE_LENGTH_FOR_CROSSING_AUTON_LINE/2 - RobotSpecifics.BACK_TO_MIDDLE);
+		DriveAuton.turn(90);
+		DriveAuton.move(FieldSpecifics.BLUE_LENGTH_OF_FIELD_FROM_DRIVER_STATION/2 - (FieldSpecifics.BLUE_LENGTH_FROM_RIGHT_WALL_TO_SWITCH - FieldSpecifics.BLUE_LENGTH_FROM_RIGHT_WALL_TO_CORNER_BEND + FieldSpecifics.BLUE_WIDTH_FOR_SWITCH/2) - FieldSpecifics.BLUE_LENGTH_FOR_VAULT_TO_CENTER);
+		DriveAuton.turn(-90);
+		DriveAuton.move(FieldSpecifics.BLUE_LENGTH_FOR_CROSSING_AUTON_LINE/2 - RobotSpecifics.FRONT_TO_MIDDLE);
+	}
+
+	static void blueRightRightForward() {
+		Intake.setArmsHardHold();
+		DriveAuton.move(FieldSpecifics.BLUE_LENGTH_FOR_CROSSING_AUTON_LINE - RobotSpecifics.BACK_TO_MIDDLE - RobotSpecifics.FRONT_TO_MIDDLE);
+	}
+
+	static void blueRightRightSwitch() {
+		Intake.setArmsHardHold();
+		DriveAuton.move(FieldSpecifics.BLUE_RIGHT_LENGTH_TO_MIDDLE_OF_SWITCH - RobotSpecifics.BACK_TO_MIDDLE);
+		DriveAuton.turn(-90);
+		DriveAuton.move(FieldSpecifics.BLUE_LENGTH_FROM_RIGHT_WALL_TO_SWITCH - FieldSpecifics.BLUE_LENGTH_FROM_RIGHT_WALL_TO_CORNER_BEND - RobotSpecifics.SIDE_TO_MIDDLE - RobotSpecifics.FRONT_TO_MIDDLE);
+	}
 }
--- src/main/java/org/usfirst/frc/team1351/robot/autonomous/FieldSpecifics.java
+++ src/main/java/org/usfirst/frc/team1351/robot/autonomous/FieldSpecifics.java
@@ -6,14 +6,19 @@ public class FieldSpecifics {
 	static final double RED_RIGHT_LENGTH_TO_MIDDLE_OF_SWITCH = -1; //168
 	static final double RED_LENGTH_FROM_LEFT_WALL_TO_SWITCH = -1; // 85.25
 	static final double RED_LENGTH_FROM_LEFT_WALL_TO_CORNER_BEND = -1; // 29.69
+	static final double RED_LENGTH_FROM_RIGHT_WALL_TO_SWITCH = -1; // 85.25
+	static final double RED_LENGTH_FROM_RIGHT_WALL_TO_CORNER_BEND = -1; // 29.69
 	static final double RED_LENGTH_OF_FIELD_FROM_DRIVER_STATION = -1; //264
 	static final double RED_LENGTH_FOR_VAULT_TO_CENTER = -1; //12
-	static final double RED_WIDTH_FOR_SWITCH = -1; //36
+	static final double RED_WIDTH_FOR_SWITCH = -1; //3;
 	static final double BLUE_LENGTH_FOR_CROSSING_AUTON_LINE = -1; //140
-	static final double BLUE_LENGTH_TO_MIDDLE_OF_SWITCH = -1; // 168
+	static final double BLUE_LEFT_LENGTH_TO_MIDDLE_OF_SWITCH = -1; // 168
+	static final double BLUE_RIGHT_LENGTH_TO_MIDDLE_OF_SWITCH = -1; //168
 	static final double BLUE_LENGTH_FROM_LEFT_WALL_TO_SWITCH = -1; // 85.25
 	static final double BLUE_LENGTH_FROM_LEFT_WALL_TO_CORNER_BEND = -1; // 29.69
+	static final double BLUE_LENGTH_FROM_RIGHT_WALL_TO_SWITCH = -1; // 85.25
+	static final double BLUE_LENGTH_FROM_RIGHT_WALL_TO_CORNER_BEND = -1; // 29.69
 	static final double BLUE_LENGTH_OF_FIELD_FROM_DRIVER_STATION = -1; //264
 	static final double BLUE_LENGTH_FOR_VAULT_TO_CENTER = -1; //12
-	static final double BLUE_WIDTH_FOR_SWITCH = -1; //36
+	static final double BLUE_WIDTH_FOR_SWITCH = -1; //3;
 }
